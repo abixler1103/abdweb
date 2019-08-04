7d710a425b9a9370aac10b92113643db355bca8a
Brenden Bixler <brenden.bixler@gmail.com>
2019-08-04 14:40:46 -0400

    Init commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+  <title>About</title>
+</svelte:head>
+
+<h1>About this site</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
+<br />
+<br />
+<a href="/">Return Home</a>
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  theme: {
+    fontSize: {
+      xs: '.75rem',
+      sm: '.875rem',
+      tiny: '.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '9xl': '6rem'
+    },
+    fontFamily: {
+      serif: ['Lato', 'Georgia']
+    },
+    extend: {
+      colors: {
+        'adb-teal': '#0086ad',
+        'adb-yellow': '#FFe831',
+        'adb-pink': '#d6144e'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
